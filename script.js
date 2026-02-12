e54b4a3d8ced4f091245afbd904bf0355b29f83b
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-12 23:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function insert_Row() {
 	let cell1 = newrow.insertCell(0);
 	let cell2 = newrow.insertCell(1);
 
-	cell1.innerHTML = "Row1 Cell1";
-	cell2.innerHTML = "Row1 Cell2";
+	cell1.innerHTML = "New Cell1";
+	cell2.innerHTML = "New Cell2";
   
 }
